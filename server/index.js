5f854f0662871ecd749b87d23a5f5b038cef319e
Daniel Pham <eetandg@mail.ccsf.edu>
2019-07-20 03:10:12 -0700

    created server and skeleton of app

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+
+const port = 1337;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.resolve(__dirname + "/dist")));
+
+app.listen(port, () => {
+  console.log("the server is connected on" + port);
+});
