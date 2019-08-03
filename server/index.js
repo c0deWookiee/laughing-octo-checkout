2535cca5d42d554a9a81e6c58c97e3308c5efa49
Daniel Pham <eetandg@mail.ccsf.edu>
2019-08-03 13:45:09 -0700

    db connecing successfully

--- server/index.js
+++ server/index.js
@@ -3,14 +3,16 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 const router = require("./routes");
-
-const port = 1337;
-
+//database
+const db = require("../db/db.js");
+//port
+const port = 1338;
+//middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static(path.resolve(__dirname + "/dist")));
-
+//router
 app.use("/api", router);
 
 app.listen(port, () => {
